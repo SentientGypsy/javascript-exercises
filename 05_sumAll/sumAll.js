7f9fcf1d9fb9eeae8af5a45a1803da93ab0b8891
sentientgypsy <25094690+SentientGypsy@users.noreply.github.com>
2024-11-01 12:13:55 -0500

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+     let min = firstNum;
+     let max = secondNum;
 
+     if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+     }
+     if (isNaN(firstNum) || isNaN(secondNum)) {
+        return "ERROR";
+     }
+     if (secondNum < 0 || firstNum < 0) {
+        return "ERROR";
+     }
+     if (secondNum < firstNum) {
+        max = firstNum; 
+        min = secondNum;
+     }
+
+
+
+    let total = 0;
+    for (i=min; i <= max; i++) {
+       total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
