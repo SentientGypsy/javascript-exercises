fb91b160e1bd131bf03590e758c52500565b1778
sentientgypsy <25094690+SentientGypsy@users.noreply.github.com>
2024-11-01 12:43:13 -0500

    complete 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  
+  let conversion  = (number - 32) * (5/9);
+
+  return Math.round(conversion * 10) / 10 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let conversion = (number * (9/5)) + 32;
+
+  return Math.round(conversion * 10) / 10 
 };
 
 // Do not edit below this line
