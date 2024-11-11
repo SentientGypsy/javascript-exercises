acf8fc38619af36d881e95a2406b9c1180d902ee
sentientgypsy <25094690+SentientGypsy@users.noreply.github.com>
2024-11-11 11:22:31 -0600

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const brokenString = string.toLowerCase().split("");
+    const filteredBrokenString = brokenString.filter((letter) => /[a-z0-9]/.test(letter));
+    return filteredBrokenString.join("") === filteredBrokenString.reverse().join("");
+
 
 };
 
 // Do not edit below this line
+
 module.exports = palindromes;
