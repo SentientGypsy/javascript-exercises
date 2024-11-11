0446f65be3816fc93a0db0a60d00d2724d6c896a
sentientgypsy <25094690+SentientGypsy@users.noreply.github.com>
2024-11-11 11:53:48 -0600

    complete fibbonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    const fibboArray = [0, 1]; // Start with the first two Fibonacci numbers
+    if (index >= 0){
+        for (let i = 2; i <= index; i++) {
+            const nextNum = fibboArray[i - 1] + fibboArray[i - 2];
+            fibboArray.push(nextNum);
+        }
+    } else {return "OOPS"}
+    return fibboArray[index];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
