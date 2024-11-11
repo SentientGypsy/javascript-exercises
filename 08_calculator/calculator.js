7442e66157320e0655b2bec2051c7aa8375c07d1
sentientgypsy <25094690+SentientGypsy@users.noreply.github.com>
2024-11-11 10:23:12 -0600

    complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(numOne, numTwo) {
+	return numOne + numTwo;
+};
+
+const subtract = function(numOne, numTwo) {
+  return numOne - numTwo;
 	
 };
 
-const subtract = function() {
+const sum = function(arr) {
+  if (arr.length !== 0 && arr.length > 1) {
+	  const sumOfArray = arr.reduce((total, currentItem) => {
+      return total + currentItem;
+    }, 0);
+    return sumOfArray;
+} else if(arr.length === 1) {
+  return arr[0];
+} else {
+    return 0;
+}
+};
+
+const multiply = function(arr) {
+  if (arr.length !== 0 && arr.length > 1) {
+	  const productOfArray = arr.reduce((total, currentItem) => {
+      return total * currentItem;
+    }, 1);
+    return productOfArray;
+} else if(arr.length === 1) {
+  return arr[0];
+} else {
+    return 0;
+}
+};
+
+const power = function(numberOne, numberTwo) {
+  return numberOne ** numberTwo;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let totalProduct = 1;
+	for(i = num; i > 0; i--){
+    totalProduct *= i;
+  }
+  return totalProduct;
 };
 
 // Do not edit below this line
